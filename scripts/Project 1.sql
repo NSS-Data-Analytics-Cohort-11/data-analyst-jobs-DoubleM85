6b9967b0ab52c92b9aaf1cd6740194335c4d7b89
DoubleM85 <matt.mason303@gmail.com>
2024-02-06 21:37:23 -0700

    Finished with Bonus

--- scripts/Project 1.sql
+++ scripts/Project 1.sql
@@ -82,3 +82,53 @@ FROM data_analyst_jobs
 WHERE review_count > 5000;
 
 --Answer: 185
+
+-- 10. Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
+
+SELECT company, avg(star_rating) AS avg_rating
+FROM data_analyst_jobs
+WHERE review_count > 5000
+GROUP BY company
+ORDER BY avg(star_rating) DESC;
+
+--Answer: General Motors @ 4.199
+
+-- 11. Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
+
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs;
+--881 total job titles
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs
+WHERE title LIKE '%Analytics%';
+
+--Answer: 754 job title containing the word 'Analyst'
+
+-- 12. How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
+
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs
+WHERE title NOT LIKE '%Analyst%'
+AND title NOT LIKE '%Analytics%';
+
+--Answer: 26
+
+-- **BONUS:**
+-- You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
+--  - Disregard any postings where the domain is NULL. 
+--  - Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
+--   - Which three industries are in the top 4 on this list? How many jobs have been listed for more than 3 weeks for each of the top 4?
+
+SELECT domain, COUNT(DISTINCT title) AS hard_to_fill
+FROM data_analyst_jobs
+WHERE skill = 'SQL'
+AND days_since_posting > 21
+AND domain IS NOT NULL
+GROUP BY domain
+ORDER BY hard_to_fill DESC;
+
+--Answer:
+-- 1. Consulting and Business Services - 4 jobs
+-- 2. Consumer Goods and Services - 2 jobs
+-- 3. Computers and Electronics - 1 job
+-- 4. Internet and Software - 1 job
